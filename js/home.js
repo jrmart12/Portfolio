427f40938b73d500df9bb80ffbeafa47a38d0663
jrmart12 <rodolfojose@live.com>
2022-02-10 20:42:06 -0600

    header complete

---
+++ js/home.js
@@ -0,0 +1,9 @@
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector(".loading").style.visibility = "visible";
+  } else {
+    document.querySelector(".loading").style.visibility = "hidden";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
