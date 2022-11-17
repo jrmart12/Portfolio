4fccbe7c816b1902666b2e9b3117ac07d158342b
jrmart12 <rodolfojose@live.com>
2022-11-17 11:48:37 -0600

    change to astro

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Jose Rodolfo Martinez';
+export const SITE_DESCRIPTION = 'Jose Rodolfo Martinez Software Developer Based in Honduras';
